118475742ede4d83de8a44648e263c0ae897e6ed
luizclaudio <luiz.silva@wisebyte.com.br>
2025-03-13 13:37:43 -0300

    Remove GreetingResource, MyEntity, and MyEntityResource classes
    Took 56 minutes

--- src/main/java/com/example/school/MyEntityResource.java
+++
@@ -1,6 +0,0 @@
-package com.example.school;
-
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-
-public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
-}
